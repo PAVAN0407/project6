70416cf01e8e77b32182989cc6c889cfe78055a3
PAVAN0407 <gnpavan2004@gmail.com>
2025-05-27 08:46:08 +0530

    First COfrcmrfmdi

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "   Hello Pavan2b  " );
+        System.out.println( "   Hello Pavan2b 2250  " );
     }
 }
